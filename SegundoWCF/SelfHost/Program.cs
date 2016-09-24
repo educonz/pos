eeda63ebc3b1f403e81c29e0486596187b1da46d
educonz <conz.eduardo@hotmail.com>
2016-09-24 10:46:06 -0300

    Finalização do segundo WCF

--- SegundoWCF/SelfHost/Program.cs
+++ SegundoWCF/SelfHost/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using WCF_Funcionario;
 
 namespace SelfHost
 {
@@ -10,6 +12,26 @@ namespace SelfHost
     {
         static void Main(string[] args)
         {
+            ServiceHost host = new ServiceHost(typeof(ServicoFuncionario));
+            host.AddServiceEndpoint(typeof(IFuncionario), new BasicHttpBinding(), new Uri("http://localhost:8081"));
+            try
+            {
+                host.Open();
+
+                Console.WriteLine($"O servico {host.Description.ServiceType} está online");
+                host.Description.Endpoints.ToList().ForEach(x =>
+                    Console.WriteLine(x.Address)
+                );
+
+                Console.ReadLine();
+                host.Close();
+            }
+            catch(Exception ex)
+            {
+                host.Abort();
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
